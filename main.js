45291a497b2250eac434276a7385bcdd89f6c289
HemanthAchu <145959875+HemanthAchu@users.noreply.github.com>
2023-12-15 18:45:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const reds =document.getElementById('red')
+
+
+function change(){
+    reds.style.backgroundColor="yellow"
+}
+function change1(){
+    reds.style.backgroundColor="green"
+}
+function change2(){
+    reds.style.backgroundColor="blue"
+}
+const change3=()=>{
+    reds.style.backgroundColor="red"
+}
